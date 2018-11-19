f37a40a0733bb3895290d09d85d443a10fb8eea2
Starwort <tcphone93@gmail.com>
2018-11-19 20:55:43 +0000

    I forgot to consider the failsafe

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ class Program:
 
         if not opts.files:
             played = 0
-            click((0,0)) # Focus the window
+            click((1,1)) # Focus the window WITHOUT triggering the failsafe
             while played < opts.games or not opts.games:
                 if played:
                     log.info('Beginning new game...')
